917439164a864222d12176dbebfa12d932f6b4e1
rde-espi <reinaldo_7531@hotmail.com>
2026-07-31 13:07:37 +0100

    First Controller

--- SuperShop/SuperShop/Startup.cs
+++ SuperShop/SuperShop/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SuperShop.Data;
 
 namespace SuperShop
 {
@@ -24,6 +21,10 @@ namespace SuperShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnections"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
