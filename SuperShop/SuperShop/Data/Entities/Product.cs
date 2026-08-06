c3350f166d13ba0abbfb115d46453ac1391d0627
rde-espi <reinaldo_7531@hotmail.com>
2026-08-06 11:25:46 +0100

    Modify Products

--- SuperShop/SuperShop/Data/Entities/Product.cs
+++ SuperShop/SuperShop/Data/Entities/Product.cs
@@ -6,6 +6,8 @@ namespace SuperShop.Data.Entities
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="The field {0} must have less than 50 characters")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -15,10 +17,10 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Available")]
