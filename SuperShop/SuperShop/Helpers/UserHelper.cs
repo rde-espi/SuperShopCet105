2dd860627cb3767e5f5b34146e30fcf6b79bf455
rde-espi <reinaldo_7531@hotmail.com>
2026-09-18 22:09:12 +0100

    Change User Profile and Password

--- SuperShop/SuperShop/Helpers/UserHelper.cs
+++ SuperShop/SuperShop/Helpers/UserHelper.cs
@@ -34,5 +34,15 @@ namespace SuperShop.Helpers
         {
             await _signInManager.SignOutAsync();
         }
+
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, oldPassword, newPassword);
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
     }
 }
---
+++ SuperShop/SuperShop/Models/ChangeUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Models
+{
+    public class ChangeUserViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirtsName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
